5e1485cecb476ba3bc7ac760cde20f1eab5f64b6
tpittma2 <tpittma2@gmail.com>
2021-09-08 10:44:50 -0400

    added more buttons

--- Scripts/etch-a-sketch.js
+++ Scripts/etch-a-sketch.js
@@ -1,11 +1,32 @@
 const boxSize = document.getElementById('box-range');
+
 const gameContainer = document.querySelector('.game-container');
 const btnClear = document.querySelector('.clear');
+const btnToggleGrid = document.querySelector('.toggle-grid');
+const colorButtons = document.querySelectorAll('color-option-button');
+const btnPenColor = document.querySelector('pen-color');
+const colorPicker = document.getElementById('pen-color');
+const ACTIVE_COLOR_CLASS = 'active-color';
 
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
+}
+
+function setColorMethod(control)
+{
+
+    colorButtons.forEach(x=> {
+        if(x == control)
+        {
+            x.classList.add(ACTIVE_COLOR_CLASS);
+        }
+        else
+        {
+            x.classList.remove(ACTIVE_COLOR_CLASS);
+        }
+    })
 }
 
 function buildGame()
@@ -21,15 +42,38 @@ function buildGame()
         {
             let newSquare = document.createElement('div')
             newSquare.classList.add('game-square');
+           // newSquare.classList.add('grid');
             newSquare.addEventListener('mouseover', function(e){ colorBox(e)});
             newRow.appendChild(newSquare);
         }
     }
 }
 
-function colorBox(e) {
-    e.target.style['background-color'] = 'black';
+function toggleGridLines() {
+    const gameSquares = document.querySelectorAll('.game-square');
+    gameSquares.forEach(square => { square.classList.toggle('grid'); })
 }
+
+function colorBox(e) {
+    e.target.style['background-color'] = getRandomColor(); //'black';
+}
+
+function setBoxSizeText()
+{
+    const boxSizeElements = document.querySelectorAll('.box-size');
+    boxSizeElements.forEach(x=> x.textContent = `${boxSize.value}x${boxSize.value}`);
+}
+
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
 
 
 
@@ -41,6 +85,10 @@ boxSize.addEventListener('input', function(e)
 });
 boxSize.addEventListener('change', buildGame);
 btnClear.addEventListener('click', buildGame);
+btnToggleGrid.addEventListener('click', toggleGridLines);
+colorButtons.forEach(x=> x.addEventListener('click', function(e){setColorMethod(e.target)}));
+
+boxSize.value = 16;
+setBoxSizeText();
 buildGame();
 
-//applyButton.addEventListener()
